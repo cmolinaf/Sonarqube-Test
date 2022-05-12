52c3e39475610a615f47f7c19d0d88da27901348
cmolinaf <104526127+cmolinaf@users.noreply.github.com>
2022-05-12 18:59:18 -0400

    Add files via upload

---
+++ src/assets/colors.ts
@@ -0,0 +1,7 @@
+export const $color = {
+  primary: "#303778",
+  secundary: "#333333",
+  success: "#34c387",
+  warning: "#ff9800",
+  danger: "#ee3f5f",
+};
---
+++ src/assets/styles.ts
@@ -0,0 +1,2 @@
+export const firstInputStyle = { marginTop: 15 };
+export const inputStyle = { marginTop: 15 };
